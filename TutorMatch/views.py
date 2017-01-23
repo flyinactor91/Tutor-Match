af10f64e4946b3b268f9edc6a4d99db796c57a08
Michael duPont <michael@mdupont.com>
2017-01-23 14:50:52 -0500

    Add skills and endpoints to filter by skill

--- TutorMatch/views.py
+++ TutorMatch/views.py
@@ -1,3 +1,4 @@
+import functools
 import sqlite3
 from flask import g, request, Response
 from TutorMatch import app
@@ -27,6 +28,22 @@ def close_connection(excp: Exception):
     if dbconn is not None:
         dbconn.close()
 
+##--- Custom Decorators ---##
+
+def output_str(func):
+    """This decorator adds the 'raw' boolean keyword to functions.
+    When applied, the function's output will be converted to a string
+    unless the function call includes raw=True
+    """
+    #This decorator allows us to use our custom decs alongside Flask's routing decs
+    @functools.wraps(func)
+    def wrapper(*args, raw: bool=False, **kwargs):
+        ret = func(*args, **kwargs)
+        if not raw:
+            ret = str(ret)
+        return ret
+    return wrapper
+
 ##--- Routing ---##
 
 @app.route('/')
@@ -34,11 +51,28 @@ def home() -> str:
     return "Hello"
 
 @app.route('/users/count')
-def get_user_total():
-    return str(query_db('SELECT count(id) FROM User', one=True)[0])
+@output_str
+def user_total() -> int:
+    return query_db('SELECT count(id) FROM User', one=True)[0]
 
 @app.route('/users/<string:utype>/count')
-def get_user_count(utype: str) -> str:
+@output_str
+def user_count_by_type(utype: str) -> int:
     query = ("SELECT count(id) FROM User WHERE utype IN "
              "(SELECT id FROM User_Type WHERE name=?)")
-    return str(query_db(query, (utype.lower(),), one=True)[0])
+    return query_db(query, (utype.lower(),), one=True)[0]
+
+@app.route('/users/with/<string:skill>/count')
+@output_str
+def user_count_by_skill(skill: str) -> int:
+    query = ("SELECT count(u.id) FROM User u, Skill s, User_Skill u_s "
+             "WHERE u.id=u_s.user_id AND u_s.skill_id=s.id AND s.name=?")
+    return query_db(query, (skill.lower(),), one=True)[0]
+
+@app.route('/users/<string:utype>/with/<string:skill>/count')
+@output_str
+def user_count_by_type_and_skill(utype: str, skill: str) -> int:
+    query = ("SELECT count(u.id) FROM User u, Skill s, User_Skill u_s "
+             "WHERE u.id=u_s.user_id AND u_s.skill_id=s.id AND s.name=? "
+             "AND u.utype IN (SELECT id FROM User_Type WHERE name=?)")
+    return query_db(query, (skill.lower(), utype.lower(),), one=True)[0]
